7c63c6d367190a2add997298018d1ff35eb435da
Margot Sylvain <margot.sylvain@gmail.com>
2016-11-24 15:30:48 +0100

    Adding data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+Ingredient.destroy_all
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
-Ingredient.create(name: "Mint Julep")
-Ingredient.create(name: "Whisky sour")
-Ingredient.create(name: "Mojito")
+Cocktail.destroy_all
+Cocktail.create(name: "Mint Julep")
+Cocktail.create(name: "Whisky sour")
+Cocktail.create(name: "Mojito")
