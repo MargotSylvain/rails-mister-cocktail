26140871a5c38323e82983e72dcb8653425bfa47
Margot Sylvain <margot.sylvain@gmail.com>
2016-11-24 14:57:06 +0100

    test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :ingredients do
-      resources :doses
-    end
-  end
+  # resources :cocktails do
+  #   resources :ingredients
+  # end
+
+  get "cocktails", to: "cocktails#index"
 
 end
