c98d6180b7b3251e9fec071504e2108edac41e88
Margot Sylvain <margot.sylvain@gmail.com>
2016-11-24 15:01:21 +0100

    Add cocktail controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,17 +1,40 @@
 class CocktailsController < ApplicationController
-def index # GET /cocktails
-  @cocktails = Cocktail.all
-end
-def show # GET /cocktails/:id
-  # @cocktail = cocktail.find(params[:id])
-end
-def new # GET /cocktails/new
-end
-def create # POST /cocktails
-end
-def edit # GET /cocktails/:id/edit
-end
-def update # PATCH /cocktails/:id
-end
-def destroy # DELETE /cocktails/:id
+  def index # GET /cocktails
+    @cocktails = Cocktail.all
+  end
+  def show # GET /cocktails/:id
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def new # GET /cocktails/new
+    @cocktail = Cocktail.new
+  end
+  def create # POST /cocktails
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+  def edit # GET /cocktails/:id/edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def update # PATCH /cocktails/:id
+    @cocktail = Cocktail.find(params[:id])
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktails_path
+    else
+      render :edit
+    end
+  end
+  def destroy # DELETE /cocktails/:id
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :address, :phone, :category)
+  end
 end
